4192fddfe9ecf59f441d8a29e1cc4e8fa55e7ac5
MatamaPeter <peter24matama@gmail.com>
2025-02-03 20:38:11 +0300

    `Refactor Entry component props to use spread syntax and dot notation`

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     return(
       <Entry
         key={data.id}
-        data={data}
+        {...data}
       />
     )
   })
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -3,18 +3,18 @@ export default function Entry(props) {
     return (
         <article className="journal-entry">
             <div className="main-image-container">
-                <img className="main-image" src={props.data.img.src} alt={props.data.img.alt}/>
+                <img className="main-image" src={props.img.src} alt={props.img.alt}/>
             </div>
             <div>
                 <div className="main-content">
                     <img src="marker.png" alt="map-icon" />
-                    <span>{props.data.country}</span>
-                        <a href={props.data.googleMapsLink}>View on Google Maps</a>
+                    <span>{props.country}</span>
+                        <a href={props.googleMapsLink}>View on Google Maps</a>
                 </div>
-                <h2>{props.data.title}</h2>
-                <p>{props.data.dates}</p>
+                <h2>{props.title}</h2>
+                <p>{props.dates}</p>
                 <p className="ellipsis">
-                    {props.data.text}
+                    {props.text}
                 </p>
             </div>
             
